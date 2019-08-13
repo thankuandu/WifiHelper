a6e83729867a7f39c59964cc8d7cc8ff67dd2f24
zzj <1239112241@qq.com>
2019-08-13 15:02:57 +0800

    first commit

---
+++ wifilibrary/src/main/java/com/zj/wifilibrary/WifiAdmin.java
@@ -0,0 +1,56 @@
+package com.zj.wifilibrary;
+
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class WifiAdmin {
+
+    //定义wifiManager对象
+    private WifiManager mWifiManager;
+    //定义wifiInfo对象
+    private WifiInfo mWifiInfo;
+    //扫描出的网络连接列表
+    private List<ScanResult> mWifiList;
+    //网络连接列表
+    private List<WifiConfiguration> mWifiConfiguration;
+    //定义一个wifiLock
+    WifiManager.WifiLock mWifiLock;
+
+    public WifiAdmin(Context context) {
+        mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        mWifiInfo = mWifiManager.getConnectionInfo();
+    }
+
+    //打开wifi
+    public void openWifi(Context context) {
+        if (!mWifiManager.isWifiEnabled()) {
+            mWifiManager.setWifiEnabled(true);
+        } else if (mWifiManager.getWifiState() == WifiManager.WIFI_STATE_ENABLING) {
+            Toast.makeText(context, "亲，Wifi正在开启，不用再开了", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "亲，Wifi已经开启,不用再开了", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void closeWifi(Context context) {
+        if (mWifiManager.isWifiEnabled()) {
+            mWifiManager.setWifiEnabled(false);
+        } else if (mWifiManager.getWifiState() == WifiManager.WIFI_STATE_DISABLED) {
+            Toast.makeText(context, "亲，Wifi已经关闭，不用再关了", Toast.LENGTH_SHORT).show();
+        } else if (mWifiManager.getWifiState() == WifiManager.WIFI_STATE_DISABLING) {
+            Toast.makeText(context, "亲，Wifi正在关闭，不用再关了", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "请重新关闭", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+}
